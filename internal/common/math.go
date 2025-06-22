eb11b14896690a79478521f53b2f944d06c39ac5
amcraig <alexandercraig@protonmail.com>
2025-06-22 14:53:45 +0100

    Add common DifferenceInts Generic

---
+++ internal/common/math.go
@@ -0,0 +1,24 @@
+package common
+
+// I am aware that these exist ynder the package: https://pkg.go.dev/golang.org/x/exp/constraints
+// But to facilitate learning and not relying on imports I'm implementing them below.
+
+type Signed interface {
+	~int | ~int8 | ~int16 | ~int32 | ~int64
+}
+
+type Unsigned interface {
+	~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr
+}
+
+type Integer interface {
+	Signed | Unsigned
+}
+
+func DifferenceInts[T Integer](a, b T) int {
+	if a > b {
+		return int(a - b)
+	} else {
+		return int(b - a)
+	}
+}
