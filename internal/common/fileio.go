430863b547141ed5a053b0d095753175271bfe89
amcraig <alexandercraig@protonmail.com>
2025-06-23 00:54:07 +0100

    Add readfileintostring util

--- internal/common/fileio.go
+++ internal/common/fileio.go
@@ -23,3 +23,24 @@ func ReadFileIntoStringSlice(filepath string) (sliceStr []string) {
 
 	return
 }
+
+func ReadFileIntoString(filepath string) string {
+	file, err := os.Open(filepath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	fscanner := bufio.NewScanner(file)
+
+	// It's ready to use from the get-go.
+	// You don't need to initialize it.
+	var strBuilder strings.Builder
+
+	for fscanner.Scan() {
+		textinput := strings.TrimSpace(fscanner.Text())
+		strBuilder.WriteString(textinput)
+	}
+
+	return strBuilder.String()
+}
---
+++ internal/common/fileio_test.go
@@ -0,0 +1,18 @@
+package common_test
+
+import (
+	"testing"
+
+	"github.com/amcraig/cryptopals-go/internal/common"
+)
+
+func TestReadFileIntoString(t *testing.T) {
+	filepath := "../../challenges/set1/testFiles/6_test.txt"
+	want := "HUIfTQsPAh9PE048GmllH0kcDk4TAQsHThsBFkU2AB4BSWQgVB0dQzNTTmVSBgBHVBwNRU0HBAxTEjwMHghJGgkRTxRMIRpHKwAFHUdZEQQJAGQmB1MANxYG"
+
+	got := common.ReadFileIntoString(filepath)
+
+	if want != got {
+		t.Errorf("File content processed does not match expected: %v", got)
+	}
+}
